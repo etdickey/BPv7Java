9ca98bbe705f60b50db645527c7de238e28b1b21
ethan_dickey1 <etdickey@gmail.com>
2022-10-14 17:55:10 -0400

    Initial commit of project structure.

---
+++ src/SendStrings/NetworkTests.java
@@ -0,0 +1,11 @@
+package SendStrings;
+
+/**
+ * todo: write authorship header
+ * todo: write purpose of this class (network testing + stats)
+ */
+public class NetworkTests {
+    public static void main(String[] args){
+        //todo
+    }
+}
