45da8638bb5da7b05ad8e73726ecc91f68b89616
Sachit <sachit.kothari@aalto.fi>
2022-10-16 18:07:24 -0400

    Initial commit

---
+++ src/DTCP/interfaces/DTCP.java
@@ -0,0 +1,10 @@
+package DTCP.interfaces;
+/*
+To Do:
+Add required parameters
+Add all functions
+ */
+public interface DTCP {
+    boolean send(/*Bundle Object*/);
+    boolean notifyBPA(/*TBD*/);
+}
