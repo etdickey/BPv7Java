0dd6ed56dfee0d33094616bcc68fa43f9d3003d4
Sachit <sachit.kothari@aalto.fi>
2022-10-30 22:04:20 -0400

    Added 4 variables (missing fragmentation) in Status Report Added record type in Admin record

--- src/BPv7/containers/AdminRecord.java
+++ src/BPv7/containers/AdminRecord.java
@@ -2,4 +2,8 @@ package BPv7.containers;
 
 
 public abstract class AdminRecord {
+
+    int recordType; /* unsigned int record type, no unsigned int in java */
+    /* recordContent in type specific manner (Unsure what type specific manner means) */
+
 }
--- src/BPv7/containers/StatusReport.java
+++ src/BPv7/containers/StatusReport.java
@@ -1,5 +1,9 @@
 package BPv7.containers;
-
+import java.time.*;
 public class StatusReport extends AdminRecord{
     /*Getters and setters for status report info*/
+    boolean statusIndicator;
+    int reasonCode;
+    NodeID sourceBundleNodeID;
+    LocalTime creationTimestamp; /* These are the four required without fragmentation. There are two more with. */
 }
