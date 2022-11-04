00e86015e3d320bebf86dd12a943c360eef056fa
Aidan Casey <aidan@aidancasey.net>
2022-11-04 18:31:35 +0000

    Commented some general structure and questions

--- src/DTCP/containers/DTCP.java
+++ src/DTCP/containers/DTCP.java
@@ -5,6 +5,13 @@ import BPv7.interfaces.NodeID;
 
 public class DTCP implements DTCP.interfaces.DTCP {
 
+
+    // Needed class variables:
+    Map<int, String> idToAddressMap; //Types may need to be changed
+    // Edge List, maybe with a PRNG list with seeds? talk to group about it.
+    // i think thats it? since it doesn't have to do routing.
+
+
     /**
      * Sends bundle to bundle node
      * @param toBeSent: bundle to be sent
@@ -13,6 +20,15 @@ public class DTCP implements DTCP.interfaces.DTCP {
     @Override
     public boolean send(Bundle toBeSent) {
         //TODO: implement sending logic
+
+        /*
+         * Structure:
+         *  - This should really just be:
+         *      + pull out destination from Bundle (its in the primary block)
+         *      + Makes sure it can reach it
+         *      + Return false if a path isn't found, otherwise send it over TCP to the IP:port gotten from nodeToNetwork
+         */
+
         return false;
     }
 
@@ -24,6 +40,16 @@ public class DTCP implements DTCP.interfaces.DTCP {
     @Override
     public boolean canReach(NodeID ID) {
         //TODO: find network health status and return status
+
+        /* 
+         * Structure:
+         *  - Determining if a link is up or not needs to be pseudo-random, cause both sides need to know if its down 
+         *    or not, so I/we will need to come up with a seed scheme I guess involving both nodes, and a time period,
+         *    and have it redetermine on that time period. All members of this network will need the same result for 
+         *    each, so probably PRNG + Config determining how we use that.
+         *  - Otherwise, this just checks if (a) there is a direct connection to that node, and (b) if its currently distrupted
+         */
+
         return false;
     }
 
@@ -35,6 +61,16 @@ public class DTCP implements DTCP.interfaces.DTCP {
     @Override
     public String nodeToNetwork(NodeID ID) {
         //TODO: get networkID from the Node object
+
+        /*  
+         * Structure:
+         *  - Read in local map of NodeID to networkID from config file
+         *  - Just look it up in the map
+         * Questions:
+         *  - NodeID is a URI I believe, based on 4.2.5
+         *  - Why is this public, assumedly its a URI/NodeID to IP address:port map, no one else needs to know it?
+         */
+
         return "[placeholder] NetworkID";
     }
 
