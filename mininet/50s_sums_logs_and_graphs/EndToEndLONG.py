900c017f66ab33f4a17bf9675bea6b0a3857d78b
Ethan Dickey <etdickey@gmail.com>
2023-09-03 23:10:45 -0400

    Fixed plots based on previous minor bug causing minor graph inaccuracies.

--- mininet/50s_sums_logs_and_graphs/EndToEndLONG.py
+++ mininet/50s_sums_logs_and_graphs/EndToEndLONG.py
@@ -12,6 +12,7 @@ import re
 import time
 import datetime
 import sys
+from math import nan
 
 #matplotlib
 from matplotlib import pyplot as plt  #plotting library
@@ -27,12 +28,68 @@ sims = []
 # sims.append((1001, "001"))
 # sims.append((1010, "010"))
 # sims.append((1011, "011"))
+
 sims.append((1100, "100"))
 sims.append((1101, "101"))
-# sims.append((1110, "110"))
+sims.append((1110, "110"))
 sims.append((1111, "111"))
+# plotTitle = 'Time vs Delay For 1111, Long Running'
+# plotTitle = 'Time vs Delay For High Density (No 110), Long Running'
+plotTitle = 'Time vs Delay For High Density, Long Running'
+# plotTitle = 'Time vs Delay For Low Density, Long Running'
+# filename = "EndToEndLONG111.png"
+# filename = "EndToEndLONGHighDensity_No110.png"
+filename = "EndToEndLONGHighDensity.png"
+# filename = "EndToEndLONGLowDensity.png"
+statusReportSize = 312 #found this out by manually checking the logs of all the sims
+MAX_TIME_GAP_TO_PLOT = 10000 #ms
+PLOT_STATUS_REPORTS = False
+
 x = []
 y = []
+xSR = []
+ySR = []
+
+def processSimData(bundles, leftShift = None):
+    # arrays to hold information about 1 simulation
+    xf = []
+    yf = []
+    min = 1000000000 if leftShift is None else leftShift #should probably be INF, in milliseconds
+
+    bundles.sort() #bundles: [(block timestamp, time since creation)]
+    for value in bundles:#find the first
+        if DEBUG: print(value)
+        timeMS = value[0] * 1000
+        xf.append(timeMS)
+        if timeMS < min:
+            min = timeMS
+        yf.append(value[1])
+    #shift all arrival times to the delta from the first received bundle
+    for i in range(len(xf)):
+        #could've used a lambda, but whatever
+        xf[i] -= min
+
+    #check for large gaps and don't fill in lines
+    prev = xf[0]
+    for i in range(1, len(xf)):
+        curr = xf[i]
+        if curr-prev > MAX_TIME_GAP_TO_PLOT:
+            xf.insert(i, nan)
+            xf.insert(i, nan)
+            yf.insert(i, nan)
+            yf.insert(i, nan)
+            i += 2
+        prev = curr
+
+
+    #debug
+    if DEBUG:
+        print(xf)
+        print(yf)
+
+    return xf, yf, min
+
+
 
 #create the one figure for all plots
 fig, ax = plt.subplots()
@@ -42,6 +99,7 @@ for f in range(len(sims)):
     #open the target record file
     file = open("logger_b.log.LONG." + str(sims[f][0]))
     bundles = []
+    bundleReports = []
     for line in file: # or stdin or whatever, could redirect file into stdin
         if "[NetStats]" in line:
             #split the line with the relevant information into time
@@ -57,7 +115,7 @@ for f in range(len(sims)):
             #grab the header and bundle info
             #sample line after "[NetStats]": "Bundle Received: from:b::to:a::creationTime:227462327::seqNum:0; Time (ms) since creation: 117; Size of bundle payload (bytes):88"
             results = re.split(';|:', parts[1]) #splits on all ":" or ";"
-            header = results[0].split(' ') #results[0] == "Bundle Received" or "Bundle Arrived" (assumption)
+            header = results[0].split(' ') #results[0] == "Bundle Received" or "Bundle Arrived" or "Bundle Sent" (assumption)
             #bundle = results[1] #unsure what this is, wrote the code many months ago but this variable doesn't appear to be used
 
             #useful validation statements
@@ -68,8 +126,11 @@ for f in range(len(sims)):
             if header[1] == "Bundle":
                 #if it is a status report about a bundle arriving or being deleted/dropped in transport
                 if header[2] == "Arrived" or header[2] == "Deleted":
-                    if DEBUG: print("  Info: " + str(currTime) + " " + str(int(results[13])))
-                    bundles.append((currTime, int(results[13])))#results[13] is time since creation
+                    if DEBUG: print("  Info: " + str(currTime) + " " + str(int(results[13])) + " size: " + str(int(results[15])))
+                    if int(results[15]) == statusReportSize: #check for status reports
+                        bundleReports.append((currTime, int(results[13])))#results[13] is time since creation
+                    else:
+                        bundles.append((currTime, int(results[13])))#results[13] is time since creation
 
             #FOR A MORE ROBUST IMPLEMENTATION, COMPLETE THE FOLLOWING STEPS:
             #todo:: most importantly, save time since creation at BPAReceiver in an array
@@ -80,37 +141,25 @@ for f in range(len(sims)):
     file.close()
 
     #plot Time From First Bundle Arrival (ms) vs Delay From Creation to End (ms)
-    # arrays to hold information about 1 simulation
-    xf = []
-    yf = []
-    min = 1000000000 #should probably be INF, in milliseconds
-    bundles.sort() #bundles: [(block timestamp, time since creation)]
-    for value in bundles:#find the first
-        if DEBUG: print(value)
-        timeMS = value[0] * 1000
-        xf.append(timeMS)
-        if timeMS < min:
-            min = timeMS
-        yf.append(value[1])
-    for i in range(len(xf)):#shift all arrival times to the delta from the first received bundle
-        #could've used a lambda, but whatever
-        xf[i] -= min
-    if DEBUG:
-        print(xf)
-        print(yf)
+    xf, yf, min = processSimData(bundles)#bundles: [(block timestamp, time since creation)]
     x.append(xf)
     y.append(yf)
+    if len(bundleReports) > 0 and PLOT_STATUS_REPORTS:
+        xfSR, yfSR, _ = processSimData(bundleReports, min)#same as bundles[]
+        xSR.append(xfSR)
+        ySR.append(yfSR)
 
 #plot all simulations at once (cleaner than doing them each iteration)
 for f in range(len(sims)):
-    ax.plot(x[f], y[f], label=sims[f][1])#plot with labels
+    ax.plot(x[f], y[f], marker=',', label=sims[f][1])#plot with labels
+    if len(bundleReports) > 0 and PLOT_STATUS_REPORTS:
+        ax.plot(xSR[f], ySR[f], linestyle='dashed', marker=',', label=sims[f][1] + " Status Reports")#plot with labels
 
 #format plot
-ax.set(xlabel='Time from first bundle arrival (ms)', ylabel='Delay from creation to end (ms)', title='Time vs Delay For High Density (No 110), Long Running')
+ax.set(xlabel=f'Time from first bundle arrival (ms)', ylabel='Delay from creation to end (ms)', title=plotTitle)
 ax.grid()
 ax.legend()
 
 #display plot
 #plt.show()#uncomment if working with jupyter, etc.
-fig.savefig("EndToEndLONGHighDensity_No110.png")#uncomment out this line to save to a file (recommended for command line)
-# fig.savefig("EndToEndLONGLowDensity.png")#uncomment out this line to save to a file (recommended for command line)
+fig.savefig(filename)#uncomment out this line to save to a file (recommended for command line)
